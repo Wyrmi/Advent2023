02de0dd6cd84df87f4a99e28bd87432499f4d643
Wyrmi <cyan.wyrmie@gmail.com>
2023-12-02 15:54:49 +0200

    day 2 part1

---
+++ day2.py
@@ -0,0 +1,32 @@
+import fileinput
+import sys
+filepath = 'Advent2023/data2.txt'
+red = 12
+green = 13
+blue = 14
+bool = False
+isok = False
+sum = 0
+for line in fileinput.input(filepath, inplace = False):
+       isok = True
+       words = line.split()
+       for w in words:
+              if w.isdigit():
+                     bool = True
+                     num = int(w)
+              elif bool:
+                     if w[0] == "b":
+                            if num > 14:
+                                   isok = False
+                     elif w[0] == "g":
+                            if num > 13:
+                                   isok = False
+                     elif w[0] == "r":
+                            if num > 12:
+                                   isok = False
+                     bool = False
+       if isok:
+              sys.stdout.write(words[1] + ' ok\n')
+              sum = sum + int(words[1].strip(":"))
+sys.stdout.write('sum is ' + str(sum))
+
