443ec07cbd86a1d0dd4f6e9dd82c529c7a85377f
Wyrmi <cyan.wyrmie@gmail.com>
2023-12-02 11:06:48 +0200

    Day 1 Part 1

--- day1.py
+++ day1.py
@@ -2,13 +2,15 @@ import fileinput
 from pathlib import Path
 import os
 import sys
-filepath = 'Advent2023/test.txt'
+filepath = 'Advent2023/data1.txt'
+newnum = 'test'
+sum = 0
 for line in fileinput.input(filepath, inplace = False):
        numbers = []
        for char in line:
               if char.isdigit():
                      numbers.append(int(char))
-       for x in numbers:
-              sys.stdout.write(str(x))
-              sys.stdout.write(" ")
-       sys.stdout.write('\n')
+       newnum = str(numbers[0]) + str(numbers[-1])
+       sum = sum + int(newnum)
+sys.stdout.write("sum is ")
+sys.stdout.write(str(sum))
