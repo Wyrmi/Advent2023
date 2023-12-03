d5c9eda7b0392d50193a116339a4c8625039a6c5
Wyrmi <cyan.wyrmie@gmail.com>
2023-12-03 16:11:33 +0200

    fix editor tab settings

--- day1.py
+++ day1.py
@@ -4,11 +4,11 @@ filepath = 'Advent2023/data1.txt'
 newnum = 'test'
 sum = 0
 for line in fileinput.input(filepath, inplace = False):
-       numbers = []
-       for char in line:
-              if char.isdigit():
-                     numbers.append(int(char))
-       newnum = str(numbers[0]) + str(numbers[-1])
-       sum = sum + int(newnum)
+    numbers = []
+    for char in line:
+        if char.isdigit():
+            numbers.append(int(char))
+    newnum = str(numbers[0]) + str(numbers[-1])
+    sum = sum + int(newnum)
 sys.stdout.write("sum is ")
 sys.stdout.write(str(sum))
--- day2.py
+++ day2.py
@@ -8,25 +8,25 @@ bool = False
 isok = False
 sum = 0
 for line in fileinput.input(filepath, inplace = False):
-       isok = True
-       words = line.split()
-       for w in words:
-              if w.isdigit():
-                     bool = True
-                     num = int(w)
-              elif bool:
-                     if w[0] == "b":
-                            if num > 14:
-                                   isok = False
-                     elif w[0] == "g":
-                            if num > 13:
-                                   isok = False
-                     elif w[0] == "r":
-                            if num > 12:
-                                   isok = False
-                     bool = False
-       if isok:
-              sys.stdout.write(words[1] + ' ok\n')
-              sum = sum + int(words[1].strip(":"))
+    isok = True
+    words = line.split()
+    for w in words:
+        if w.isdigit():
+            bool = True
+            num = int(w)
+        elif bool:
+            if w[0] == "b":
+                if num > 14:
+                    isok = False
+            elif w[0] == "g":
+                if num > 13:
+                    isok = False
+            elif w[0] == "r":
+                if num > 12:
+                    isok = False
+            bool = False
+    if isok:
+        sys.stdout.write(words[1] + ' ok\n')
+        sum = sum + int(words[1].strip(":"))
 sys.stdout.write('sum is ' + str(sum))
 
