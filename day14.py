bde198fe0781a785a86d199b03ccc402e7439dc3
Wyrmi <cyan.wyrmie@gmail.com>
2023-12-15 13:39:46 +0200

    day 14 part 1

---
+++ day14.py
@@ -0,0 +1,32 @@
+import fileinput
+filepath = 'Advent2023/data14.txt'
+sum = 0
+listlist = []
+i = 0
+
+#parse puzzle input into a 2d list
+for line in fileinput.input(filepath, inplace = False):
+    listlist.append([])
+    for char in line:
+        if char != '\n':
+            listlist[i].append(char)
+    i = i + 1
+
+#drop Os to north if . on top
+for y in range(1, len(listlist)):
+    for x in range(len(listlist[y])):
+        j = y
+        if listlist[y][x] == 'O':
+            while listlist[j - 1][x] == '.':
+                if j==0:
+                    break
+                listlist[j][x] ='.'
+                j = j - 1
+                listlist[j][x] ='O'
+
+#sum up the Os according to vertical position
+for y in range(len(listlist)):
+    for x in range(len(listlist[y])):
+        if listlist[y][x] == 'O':
+            sum = sum + (len(listlist)-y)
+print(sum)
